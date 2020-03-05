1b8bb41590b0529f8e9de738bf16b8cfd1f42c04
Charles L. Berube <charleslberube@gmail.com>
2020-03-05 14:52:37 -0500

    Add PolynomialDecomposition class, cython modules and plot lib

--- setup.py
+++ setup.py
@@ -19,12 +19,16 @@ from distutils.extension import Extension
 from distutils.command.sdist import sdist as _sdist
 import numpy
 
+PACKAGES = find_packages(where="src")
+
+
 class sdist(_sdist):
     def run(self):
         # Make sure the compiled Cython files in the distribution are up-to-date
         from Cython.Build import cythonize
         cythonize(['./src/bisip/cython_funcs.pyx'])
         _sdist.run(self)
+
 
 try:
     from Cython.Distutils import build_ext
@@ -33,28 +37,27 @@ except ImportError:
 else:
     use_cython = True
 
-cmdclass = { }
-ext_modules = [ ]
+cmdclass = {}
+ext_modules = []
 
 cmdclass['sdist'] = sdist
 
 if use_cython:
     ext_modules += [
-        Extension("bisip.cython_funcs", [ "./src/bisip/cython_funcs.pyx" ]),
+        Extension("bisip.cython_funcs", ["./src/bisip/cython_funcs.pyx"]),
     ]
-    cmdclass.update({ 'build_ext': build_ext })
-    print("up")
+    cmdclass.update({'build_ext': build_ext})
 else:
     ext_modules += [
-        Extension("bisip.cython_funcs", [ "./src/bisip/cython_funcs.c" ],
+        Extension("bisip.cython_funcs", ["./src/bisip/cython_funcs.c"],
                   include_dirs=[numpy.get_include()]),
     ]
-    print("down")
 
 setup(
-  name = 'bisip2',
-  packages=['bisip2',], # this must be the same as the name above
-  # py_models=['models','invResults','GUI'],
+  name = 'bisip',
+  packages=PACKAGES, # this must be the same as the name above
+  package_dir={"": "src"},
+  # py_models=['models','plotlib','utils'],
   version = '0.0.1',
   license = 'MIT',
   install_requires=['emcee', 'numpy', 'matplotlib'],
