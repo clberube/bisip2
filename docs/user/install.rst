820a1912d073cbcb5bb50ab2a378c8601066a2c4
clberube <charleslberube@gmail.com>
2020-03-12 18:56:17 -0400

    Simplify setup.py with cython extensions

--- docs/user/install.rst
+++ docs/user/install.rst
@@ -3,15 +3,17 @@
 Installation
 ============
 
-BISIP is compatible with Python 3.7.
-You will also need the following packages:
+BISIP is compatible with Python 3.7+
+Installing BISIP will also install the following required packages:
 
 - `numpy <https://numpy.org/>`_
+- `cython <https://cython.org/>`_
 - `matplotlib <https://matplotlib.org/>`_
 - `emcee <https://emcee.readthedocs.io/en/stable/>`_
 
-And optionally the corner package to draw corner plots:
+And optionally the `tqdm` and `corner` packages for progress bars and corner plots.
 
+- `tqdm <https://tqdm.github.io/>`_
 - `corner <https://corner.readthedocs.io/en/latest/>`_
 
 Package managers
--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@
 # @Author: cberube
 # @Date:   05-03-2020
 # @Email:  charles@goldspot.ca
-# @Last modified by:   cberube
-# @Last modified time: 12-03-2020
+# @Last modified by:   charles
+# @Last modified time: 2020-03-12T18:52:48-04:00
 # -*- coding: utf-8 -*-
 """
 Created on Fri Sep 29 16:18:50 2017
@@ -14,62 +14,36 @@ Created on Fri Sep 29 16:18:50 2017
 
 from setuptools import setup, find_packages
 
-# from distutils.core import setup
 from distutils.extension import Extension
-from distutils.command.sdist import sdist as _sdist
-import numpy
 
 
 SRC_DIR = 'src'
 PACKAGES = find_packages(where=SRC_DIR)
-
-
-class sdist(_sdist):
-    def run(self):
-        from Cython.Build import cythonize
-        cythonize(['./src/bisip/cython_funcs.pyx'])
-        _sdist.run(self)
-
-
-try:
-    from Cython.Distutils import build_ext
-except ImportError:
-    use_cython = False
-else:
-    use_cython = True
+PREREQ = ['setuptools>=18.0', 'cython']
+REQUIRES = ['emcee', 'corner', 'numpy', 'matplotlib', 'tqdm']
 
 cmdclass = {}
-ext_modules = []
-
-cmdclass['sdist'] = sdist
-
-if use_cython:
-    ext_modules += [
-        Extension("bisip.cython_funcs", ["./src/bisip/cython_funcs.pyx"]),
-    ]
-    cmdclass.update({'build_ext': build_ext})
-else:
-    ext_modules += [
-        Extension("bisip.cython_funcs", ["./src/bisip/cython_funcs.c"],
-                  include_dirs=[numpy.get_include()]),
-    ]
+EXT_MODULES = [Extension("bisip.cython_funcs",
+                         sources=["./src/bisip/cython_funcs.pyx"])]
 
 setup(
-  name='bisip',
-  packages=PACKAGES,
-  package_dir={"": SRC_DIR},
-  version='0.0.1',
-  license='MIT',
-  install_requires=['emcee', 'corner', 'numpy', 'matplotlib'],
-  description='Bayesian inversion of SIP data',
-  long_description='README.md',
-  author='Charles L. Berube',
-  author_email='charleslberube@gmail.com',
-  url='https://github.com/clberube/bisip2',
-  keywords=['stochastic inversion', 'spectral induced polarization', 'mcmc'],
-  classifiers=[],
-  cmdclass=cmdclass,
-  ext_modules=ext_modules,
-  include_dirs=[numpy.get_include()],
-  include_package_data=True,
+    name='bisip',
+    setup_requires=PREREQ,
+    packages=PACKAGES,
+    package_dir={"": SRC_DIR},
+    version='0.0.1',
+    license='MIT',
+    install_requires=REQUIRES,
+    description='Bayesian inversion of SIP data',
+    long_description='README.md',
+    author='Charles L. Berube',
+    author_email='charleslberube@gmail.com',
+    url='https://github.com/clberube/bisip2',
+    keywords=['stochastic inversion', 'spectral induced polarization', 'mcmc'],
+    classifiers=[],
+    cmdclass=cmdclass,
+    ext_modules=EXT_MODULES,
+    # include_dirs=[numpy.get_include()],
+    include_package_data=True,
+    package_data={'': ['data/*.dat']},
 )
